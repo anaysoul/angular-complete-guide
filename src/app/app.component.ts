29642b64bad6a8c8a75400c54cd70ccfad6fc9a2
anaysoul <soulyana_lakew@outlook.com>
2022-03-16 07:17:31 -0400

    setting up the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,5 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // styles: [
-  //   `
-  //     h3 {
-  //       color: dodgerblue;
-  //     }
-  //   `,
-  // ],
 })
-export class AppComponent {
-  title = 'angular-complete-guide';
-  display = false;
-  displayDetails: Array<Number> = [];
-  counter = 0;
-
-  onDisplayDetails() {
-    this.display = !this.display;
-    this.displayDetails.push(new Date().getTime());
-    console.log(this.displayDetails);
-  }
-}
+export class AppComponent {}
