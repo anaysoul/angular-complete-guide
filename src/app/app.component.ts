8d1fac44648973f8dc6d551bf4ede89bce46a59c
anaysoul <soulyana_lakew@outlook.com>
2022-03-15 23:15:00 -0400

    practicing directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [
-    `
-      h3 {
-        color: dodgerblue;
-      }
-    `,
-  ],
+  styleUrls: ['./app.component.css'],
+  // styles: [
+  //   `
+  //     h3 {
+  //       color: dodgerblue;
+  //     }
+  //   `,
+  // ],
 })
 export class AppComponent {
   title = 'angular-complete-guide';
+  display = false;
+  displayDetails: Array<Number> = [];
+  counter = 0;
+
+  onDisplayDetails() {
+    this.display = !this.display;
+    this.displayDetails.push(Date.now());
+    console.log(this.displayDetails);
+  }
 }
